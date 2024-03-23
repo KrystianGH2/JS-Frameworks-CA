1629ec4059fbc020b9737e4812cf5fd1d45b2469
Krystian Gere <kgcdronio@gmail.com>
2024-03-23 19:37:22 +0100

    Pages

---
+++ app/constants.js
@@ -0,0 +1,22 @@
+export const navigationLinks = [
+    {
+        id: "home",
+        name: "Home",
+        link: "/",
+    },
+    {
+        id: "about",
+        name: "About",
+        link: "/about",
+    },
+    {
+        id: "contact",
+        name: "Contact",
+        link: "/contact",
+    },
+    {
+        id: "product",
+        name: "Product",
+        link: "/product",
+    }
+]
